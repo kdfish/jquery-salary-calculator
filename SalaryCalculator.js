77f2238ad51af7a25dd8cf07a036a3e11c46d3c6
Karla Fish <karla.d.fish@gmail.com>
2020-03-07 18:19:17 -0600

    adding data

--- SalaryCalculator.js
+++ SalaryCalculator.js
@@ -4,6 +4,12 @@ $(document).ready(onReady);
 
 function onReady(){
 
+    let total = 0;
+    console.log(total);
+
+    $("#Total").text(total);
+
+
   $('#btn-submit').on('click', handleClick);
 
   function handleClick( event ){
@@ -16,8 +22,21 @@ function onReady(){
     let annualSalary = $("#annualSalary").val();
 
     console.log('Got inputs:', firstName, lastName, id, title, annualSalary);
+   
+    total += (annualSalary / 12);
+    $("#Total").text(total);
 
 
+    $( '#employees' ).append(`
+    <tr>
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${id}</td>
+    <td>${title}</td> 
+    <td>${annualSalary}</td>
+    <td><button type="submit" form="form1" value="Submit">Delete</button></td>
+  </tr>
+    `);
 
 
   }
