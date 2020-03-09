a2590bfab9c54ab9468b7ad43ada4a823d60ed96
Karla Fish <karla.d.fish@gmail.com>
2020-03-08 20:11:28 -0500

    salary assignment

--- SalaryCalculator.js
+++ SalaryCalculator.js
@@ -1,19 +1,17 @@
-console.log("Js")
+console.log("Js");
 
 $(document).ready(onReady);
 
-function onReady(){
+function onReady() {
+  let total = 0;
+  console.log(total);
 
-    let total = 0;
-    console.log(total);
+  $("#Total").text(total);
 
-    $("#Total").text(total);
+  $("#btn-submit").on("click", handleClick);
 
-
-  $('#btn-submit').on('click', handleClick);
-
-  function handleClick( event ){
-    console.log('in handleClick');
+  function handleClick(event) {
+    console.log("in handleClick");
 
     let firstName = $("#firstName").val();
     let lastName = $("#lastName").val();
@@ -21,24 +19,37 @@ function onReady(){
     let title = $("#title").val();
     let annualSalary = $("#annualSalary").val();
 
-    console.log('Got inputs:', firstName, lastName, id, title, annualSalary);
-   
-    total += (annualSalary / 12);
+    console.log("Got inputs:", firstName, lastName, id, title, annualSalary);
+
+    total += annualSalary / 12;
+    // If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
+    const monthlyCost = 20000;
+    if (total > monthlyCost) {
+      $("#Total").css("background-color", "red");
+    }
+
     $("#Total").text(total);
 
-
-    $( '#employees' ).append(`
+    $("#employees").append(`
     <tr>
     <td>${firstName}</td>
     <td>${lastName}</td>
     <td>${id}</td>
     <td>${title}</td> 
     <td>${annualSalary}</td>
-    <td><button type="submit" form="form1" value="Submit">Delete</button></td>
+    <td><button type="submit" class="delete" form="form1" value="Submit">Delete</button></td>
   </tr>
     `);
 
+    $(".delete").on("click", handleDeleteClick);
 
+    function handleDeleteClick(event) {
+      console.log("in handleDeleteClick");
+
+      
+       $(event.target).parent()
+        .parent()
+        .remove();
+    }
   }
-
 }
